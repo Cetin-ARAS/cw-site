c3ba50cbfc861669875f00f93ef0da1041b7f0e8
ÇETİN <cetinnnarasss@gmail.com>
2022-07-05 22:49:42 +0300

    cont

---
+++ src/components/styles/Header.style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+backgroubd-color: ${({ theme}) => theme.colors.header};
+
+  padding: 4rem 0;
+`;
+
+export default StyledHeader;
